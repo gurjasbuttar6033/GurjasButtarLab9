54ee8121cd71a0fedd44d6dc6c6264213805a35e
GurjasButtar2033 <gpurji@gmail.com>
2025-03-18 14:57:46 -0400

    tried implementing navigation drawer

--- GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
+++ GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
@@ -8,19 +8,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ButtarActivity9 extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.gurmain);
+        navigationView = findViewById(R.id.gurnav_view);
+
         if (savedInstanceState == null) {
-            loadFragment(new Gurjas1Fragment());
+            loadFragment(new Fragment1());
         }
     }
 
--- GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/Fragment2.java
+++ GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/Fragment2.java
@@ -8,14 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Gurjas1Fragment extends Fragment {
+
+public class Fragment2 extends Fragment {
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gurjas1, container, false);
+
+        return inflater.inflate(R.layout.fragment_2, container, false);
     }
 }
