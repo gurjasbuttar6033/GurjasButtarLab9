168f7a98c6dc1637ab90445ea7413b53512250de
GurjasButtar2033 <gpurji@gmail.com>
2025-03-18 13:56:59 -0400

    edited splash screen

--- GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
+++ GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
@@ -8,18 +8,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ButtarActivity9 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gurmain), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        if (savedInstanceState == null) {
+            loadFragment(new Gurjas1Fragment());
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.gurfragment_container, fragment);
+        transaction.commit();
     }
 }
