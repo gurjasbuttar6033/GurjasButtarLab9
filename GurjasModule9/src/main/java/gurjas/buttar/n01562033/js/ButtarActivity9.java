6835cbc6584e7837f65aed771f5b10946ff9283e
GurjasButtar2033 <gpurji@gmail.com>
2025-03-19 14:28:50 -0400

    made proper nav drawer

--- GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
+++ GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
@@ -6,9 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -21,18 +24,59 @@ import com.google.android.material.navigation.NavigationView;
 public class ButtarActivity9 extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.gurtoolbar);
+        Toolbar toolbar = findViewById(R.id.gurtoolbar);
         setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.gurmain);
+        NavigationView navigationView = findViewById(R.id.gurnav_view);
+        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.gurfragment_container, new Gurjas1Fragment()).commit();
+            navigationView.setCheckedItem(R.id.gur_nav_fragone);
+
+        }
     }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.gur_nav_fragone) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.gurfragment_container, new Gurjas1Fragment()).commit();
+        } else if (itemId == R.id.but_nav_fragtwo) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.gurfragment_container, new Buttar2Fragment()).commit();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+            public void onBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    super.onBackPressed();
+                }
+            }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
