07bd89c4a88e564cbe35a7a02b158e9fcac8cafc
GurjasButtar2033 <gpurji@gmail.com>
2025-03-19 15:39:13 -0400

    added functionalities for gur and but sub-menu items

--- GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
+++ GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/ButtarActivity9.java
@@ -1,14 +1,25 @@
 //Gurjas Buttar N01562033
 package gurjas.buttar.n01562033.js;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
@@ -46,7 +57,14 @@ public class ButtarActivity9 extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.gurfragment_container, new Gurjas1Fragment()).commit();
             navigationView.setCheckedItem(R.id.gur_nav_fragone);
+            SharedPreferences prefs = getSharedPreferences(getString(R.string.settings), MODE_PRIVATE);
+            boolean isDarkMode = prefs.getBoolean(getString(R.string.dark_mode), false);
 
+            if (isDarkMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
         }
     }
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -61,6 +79,74 @@ public class ButtarActivity9 extends AppCompatActivity {
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.gur_toggle) {
+            toggleDarkMode();
+            return true;
+        } else if (id == R.id.gur_search) {
+            showSearchDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void toggleDarkMode() {
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.settings), MODE_PRIVATE);
+        boolean isDarkMode = prefs.getBoolean(getString(R.string.dark_mode), false);
+
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(getString(R.string.dark_mode), !isDarkMode);
+        editor.apply();
+    }
+
+    private void showSearchDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.search);
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton(getString(R.string.search), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String query = input.getText().toString().trim();
+                if (!query.isEmpty()) {
+                    launchGoogleSearch(query);
+                }
+            }
+        });
+
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+    private void launchGoogleSearch(String query) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null && getCurrentFocus() != null) {
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=" + Uri.encode(query)));
+        startActivity(intent);
+    }
+
+
+
 
 
     @Override
