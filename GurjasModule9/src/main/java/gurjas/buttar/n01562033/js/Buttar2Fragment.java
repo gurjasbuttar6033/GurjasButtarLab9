17a4e83ff27ad182c7622478eb99e77e9f4ab5fe
GurjasButtar2033 <gpurji@gmail.com>
2025-03-21 19:32:36 -0400

    added images for readme

--- GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/Buttar2Fragment.java
+++ GurjasModule9/src/main/java/gurjas/buttar/n01562033/js/Buttar2Fragment.java
@@ -3,11 +3,14 @@ package gurjas.buttar.n01562033.js;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -25,123 +28,72 @@ public class Buttar2Fragment extends Fragment {
     private ToggleButton fileType;
     private EditText fileName, fileContents;
 
+    @Nullable
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_buttar2);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_buttar2, container, false);
 
-        fileName = findViewById(R.id.activity_internalstorage_filename);
-        fileContents = findViewById(R.id.activity_internalstorage_filecontents);
-
-        fileType = findViewById(R.id.activity_internalstorage_filetype);
+        fileName = view.findViewById(R.id.activity_internalstorage_filename);
+        fileContents = view.findViewById(R.id.activity_internalstorage_filecontents);
+        fileType = view.findViewById(R.id.activity_internalstorage_filetype);
         fileType.setChecked(true);
 
-        findViewById(R.id.activity_internalstorage_create).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createFile(getApplicationContext(), fileType.isChecked());
-            }
-        });
+        view.findViewById(R.id.activity_internalstorage_create).setOnClickListener(v -> createFile(requireContext(), fileType.isChecked()));
+        view.findViewById(R.id.activity_internalstorage_delete).setOnClickListener(v -> deleteFile(requireContext(), fileType.isChecked()));
+        view.findViewById(R.id.activity_internalstorage_write).setOnClickListener(v -> writeFile(requireContext(), fileType.isChecked()));
+        view.findViewById(R.id.activity_internalstorage_read).setOnClickListener(v -> readFile(requireContext(), fileType.isChecked()));
 
-        findViewById(R.id.activity_internalstorage_delete).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deleteFile(getApplicationContext(), fileType.isChecked());
-            }
-        });
-
-        findViewById(R.id.activity_internalstorage_write).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                writeFile(getApplicationContext(), fileType.isChecked());
-            }
-        });
-
-        findViewById(R.id.activity_internalstorage_read).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                readFile(getApplicationContext(), fileType.isChecked());
-            }
-        });
+        return view;
     }
 
     private void createFile(Context context, boolean isPersistent) {
-        File file;
-        if (isPersistent) {
-            file = new File(context.getFilesDir(), fileName.getText().toString());
-        } else {
-            file = new File(context.getCacheDir(), fileName.getText().toString());
-        }
+        File file = isPersistent ? new File(context.getFilesDir(), fileName.getText().toString()) : new File(context.getCacheDir(), fileName.getText().toString());
 
         if (!file.exists()) {
             try {
                 file.createNewFile();
-                Toast.makeText(context, String.format("File %s has been created", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, getString(R.string.file) + fileName.getText().toString() + getString(R.string.has_been_created), Toast.LENGTH_SHORT).show();
             } catch (IOException e) {
-                Toast.makeText(context, String.format("File %s creation failed", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, getString(R.string.file_creation_failed), Toast.LENGTH_SHORT).show();
             }
         } else {
-            Toast.makeText(context, String.format("File %s already exists", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
+            Toast.makeText(context,getString(R.string.file_already_exists), Toast.LENGTH_SHORT).show();
         }
     }
 
     private void writeFile(Context context, boolean isPersistent) {
-        try {
-            FileOutputStream fileOutputStream;
-            if (isPersistent) {
-                fileOutputStream = context.openFileOutput(fileName.getText().toString(), Context.MODE_PRIVATE);
-            } else {
-                File file = new File(context.getCacheDir(), fileName.getText().toString());
-                fileOutputStream = new FileOutputStream(file);
-            }
-            fileOutputStream.write(fileContents.getText().toString().getBytes(Charset.forName("UTF-8")));
-            Toast.makeText(context, String.format("Write to %s successful", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
+        try (FileOutputStream fileOutputStream = isPersistent ? context.openFileOutput(fileName.getText().toString(), Context.MODE_PRIVATE) : new FileOutputStream(new File(context.getCacheDir(), fileName.getText().toString()))) {
+            fileOutputStream.write(fileContents.getText().toString().getBytes(Charset.forName(getString(R.string.utf_8))));
+            Toast.makeText(context, getString(R.string.write_successful), Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(context, String.format("Write to file %s failed", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, getString(R.string.write_failed), Toast.LENGTH_SHORT).show();
         }
     }
 
     private void readFile(Context context, boolean isPersistent) {
-        try {
-            FileInputStream fileInputStream;
-            if (isPersistent) {
-                fileInputStream = context.openFileInput(fileName.getText().toString());
-            } else {
-                File file = new File(context.getCacheDir(), fileName.getText().toString());
-                fileInputStream = new FileInputStream(file);
-            }
+        try (FileInputStream fileInputStream = isPersistent ? context.openFileInput(fileName.getText().toString()) : new FileInputStream(new File(context.getCacheDir(), fileName.getText().toString()));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream, Charset.forName("UTF-8")))) {
 
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, Charset.forName("UTF-8"));
-            List<String> lines = new ArrayList<String>();
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
-            while (line != null) {
+            List<String> lines = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
                 lines.add(line);
-                line = reader.readLine();
             }
             fileContents.setText(TextUtils.join("\n", lines));
-            Toast.makeText(context, String.format("Read from file %s successful", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, getString(R.string.read_successful), Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(context, String.format("Read from file %s failed", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
-            fileContents.setText("");
-
+            Toast.makeText(context, getString(R.string.read_failed), Toast.LENGTH_SHORT).show();
+            fileContents.setText(R.string.comma);
         }
     }
 
     private void deleteFile(Context context, boolean isPersistent) {
-        File file;
-        if (isPersistent) {
-            file = new File(context.getFilesDir(), fileName.getText().toString());
+        File file = isPersistent ? new File(context.getFilesDir(), fileName.getText().toString()) : new File(context.getCacheDir(), fileName.getText().toString());
+
+        if (file.exists() && file.delete()) {
+            Toast.makeText(context, getString(R.string.file_deleted), Toast.LENGTH_SHORT).show();
         } else {
-            file = new File(context.getCacheDir(), fileName.getText().toString());
-        }
-        if (file.exists()) {
-            file.delete();
-            Toast.makeText(context, String.format("File %s has been deleted", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(context, String.format("File %s doesn't exist", fileName.getText().toString()), Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, getString(R.string.file_doesn_t_exist), Toast.LENGTH_SHORT).show();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Gurjas Buttar N01562033
-##
+## in this lab we made a fragment where we saved/added course names and their description. As well as creating files, deleting files, writing files, and reading files in the second fragment
 ### https://github.com/gurjasbuttar6033/GurjasButtarLab9
-####
+#### ![splash screen](GurjasModule9/res/drawable/rain.png)
+##### ![fragment 1](GurjasModule9/res/drawable/fragment1.png)
+###### ![fragment 2](GurjasModule9/res/drawable/fragment2.png)
